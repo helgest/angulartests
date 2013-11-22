d3bf10995cc6a2e523b0739f57d1a2fd310a5a97
Helge Standal <helge.standal@makingwaves.no>
2013-11-22 10:10:57 +0100

    New test

---
+++ Angular02/main.js
@@ -0,0 +1,9 @@
+function StartupController($scope){
+	$scope.funding = { startingEstimate:0 };
+
+	var computeNeeded = function() {
+		$scope.funding.needed = $scope.funding.startingEstimate * 10;
+	};
+
+	$scope.$watch('funding.startingEstimate', computeNeeded);
+}
