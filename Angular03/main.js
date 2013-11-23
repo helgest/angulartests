02f1f9299ebe817f2ca120142eb56403c734ebde
Helge Standal <helge.standal@makingwaves.no>
2013-11-23 12:31:01 +0100

    Table example

---
+++ Angular03/main.js
@@ -0,0 +1,9 @@
+var album = [
+	{name : 'Southwest Serenade', duration : '2:34'},
+	{name : 'Northen Light', duration : '4:11'},
+	{name : 'Western girls', duration : '0:54'}
+];
+
+function AlbumController($scope){
+	$scope.album = album;
+};
