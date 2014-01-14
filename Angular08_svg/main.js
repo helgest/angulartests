367740619bcf53a8874866bd413f34f77313a89c
Helge Standal <helge.standal@gmail.com>
2014-01-14 14:23:05 +0100

    new project

---
+++ Angular08_svg/main.js
@@ -0,0 +1,10 @@
+var circles = [
+  	{'x': 115, 'y': 120, 'r':15},
+  	{'x': 150, 'y': 160, 'r':20},
+  	{'x': 180, 'y': 110, 'r':10},
+  ];
+
+function AlbumController($scope){
+	$scope.circles = circles;
+
+};
