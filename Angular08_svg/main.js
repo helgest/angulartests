207d5d3be8f692ba9ff3558d5635f8e96c39e12d
Helge Standal <helge.standal@makingwaves.no>
2014-01-15 16:26:47 +0100

    ng-click

--- Angular08_svg/main.js
+++ Angular08_svg/main.js
@@ -1,11 +1,18 @@
 var circles = [
   	{'x': 115, 'y': 120, 'fill':'#bbaada'},
   	{'x': 150, 'y': 160, 'fill':'red'},
-  	{'x': 180, 'y': 110, 'fill':'#fedbaa'},
+  	{'x': 230, 'y': 220, 'fill':'#ffeedd'},
+  	{'x': 200, 'y': 60, 'fill':'#abdefe'},
+  	{'x': 180, 'y': 110, 'fill':'#fedbaa'}
   ];
 
-function AlbumController($scope){
+function myController($scope){
 	$scope.scaleSize = .2;
 	$scope.circles = circles;
+
+	$scope.change = function() {
+		this.circle.fill='#'+Math.random().toString(16).substr(-6); //random color
+		console.log(this.$index);
+	};
 
 };
